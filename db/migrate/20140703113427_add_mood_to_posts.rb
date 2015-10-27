fd1317f1d392dfc27fd642faaaee46fd1d52aa2c
Victor Yao <victorpx2016@icloud.com>
2015-10-27 19:03:42 +0800

    v. BETA 1.1.3
    Fixed some issues with the migration files.

--- db/migrate/20140703113427_add_mood_to_posts.rb
+++ db/migrate/20140703113427_add_mood_to_posts.rb
@@ -1,5 +1,5 @@
 class AddMoodToPosts < ActiveRecord::Migration
   def change
-    add_column :posts, :mood, :string, :default => 'not sure'
+    add_column :posts, :mood, :string, :default => 'Not Sure'
   end
 end
